9d060a6811ff408a7ce3845b1d0a3c656648ca53
Manu <ebeloin@hotmail.com>
2015-09-07 13:32:01 -0400

    presque fini petit bug

--- app/src/main/java/com/example/manu/exercice2/MainActivity.java
+++ app/src/main/java/com/example/manu/exercice2/MainActivity.java
@@ -12,99 +12,98 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void arnoldClick(View view)
-    {
-            ImageButton ima1 = (ImageButton) findViewById(R.id.IBTN_Arnold);
-            TextView couleurPanel = (TextView) findViewById(R.id.the_quote);
-            String quote = "";
-            Random r = new Random();
-            int rand = r.nextInt(5) + 1;
-            int lastRand = -1;
-            if(ima1.isPressed())
-            {
-                while(rand == lastRand)
-                {
-                    rand = r.nextInt(5) + 1;
-                }
-                lastRand = rand;
-
-                switch(rand)
-                {
-                    case 1: quote=getString(R.string.arnold_1);
-                        break;
-                    case 2: quote=getString(R.string.arnold_2);
-                        break;
-                    case 3: quote=getString(R.string.arnold_3);
-                        break;
-                    case 4: quote=getString(R.string.arnold_4);
-                        break;
-                    case 5: quote=getString(R.string.arnold_5);
-                        break;
-                }
-                couleurPanel.setText(quote);
+    public void arnoldClick(View view) {
+        ImageButton ima1 = (ImageButton) findViewById(R.id.IBTN_Arnold);
+        TextView couleurPanel = (TextView) findViewById(R.id.the_quote);
+        String quote = "";
+        Random r = new Random();
+        int rand = r.nextInt(5) + 1;
+        int lastRand = -1;
+            while (rand == lastRand) {
+                rand = r.nextInt(5) + 1;
             }
+            lastRand = rand;
+
+            switch (rand) {
+                case 1:
+                    quote = getString(R.string.arnold_1);
+                    break;
+                case 2:
+                    quote = getString(R.string.arnold_2);
+                    break;
+                case 3:
+                    quote = getString(R.string.arnold_3);
+                    break;
+                case 4:
+                    quote = getString(R.string.arnold_4);
+                    break;
+                case 5:
+                    quote = getString(R.string.arnold_5);
+                    break;
+            }
+            couleurPanel.setText(quote);
     }
 
-    public void cageClick(View view)
-    {
+    public void cageClick(View view) {
         ImageButton ima2 = (ImageButton) findViewById(R.id.IBTN_Cage);
         TextView couleurPanel = (TextView) findViewById(R.id.the_quote);
         String quote = "";
         Random r = new Random();
         int rand = r.nextInt(5) + 1;
         int lastRand = -1;
-        if(ima2.isPressed())
-        {
-            while(rand == lastRand)
-            {
-                rand = r.nextInt(5) + 1;
-            }
-            lastRand = rand;
-
-            switch(rand)
-            {
-                case 1: quote=getString(R.string.cage_1);
-                    break;
-                case 2: quote=getString(R.string.cage_2);
-                    break;
-                case 3: quote=getString(R.string.cage_3);
-                    break;
-                case 4: quote=getString(R.string.cage_4);
-                    break;
-                case 5: quote=getString(R.string.cage_5);
-                    break;
-            }
-            couleurPanel.setText(quote);
+        while (rand == lastRand) {
+            rand = r.nextInt(5) + 1;
         }
+        lastRand = rand;
+
+        switch (rand) {
+            case 1:
+                quote = getString(R.string.cage_1);
+                break;
+            case 2:
+                quote = getString(R.string.cage_2);
+                break;
+            case 3:
+                quote = getString(R.string.cage_3);
+                break;
+            case 4:
+                quote = getString(R.string.cage_4);
+                break;
+            case 5:
+                quote = getString(R.string.cage_5);
+                break;
+        }
+        couleurPanel.setText(quote);
     }
 
-    public void queenClick(View view)
-    {
+    public void queenClick(View view) {
         ImageButton ima3 = (ImageButton) findViewById(R.id.IBTN_Queen);
         TextView couleurPanel = (TextView) findViewById(R.id.the_quote);
         String quote = "";
         Random r = new Random();
         int rand = r.nextInt(5) + 1;
         int lastRand = -1;
-        if(ima3.isPressed())
-        {
-            while(rand == lastRand)
-            {
+        if (ima3.isPressed()) {
+            while (rand == lastRand) {
                 rand = r.nextInt(5) + 1;
             }
             lastRand = rand;
 
-            switch(rand)
-            {
-                case 1: quote=getString(R.string.queen_1);
+            switch (rand) {
+                case 1:
+                    quote = getString(R.string.queen_1);
                     break;
-                case 2: quote=getString(R.string.queen_2);
+                case 2:
+                    quote = getString(R.string.queen_2);
                     break;
-                case 3: quote=getString(R.string.queen_3);
+                case 3:
+                    quote = getString(R.string.queen_3);
                     break;
-                case 4: quote=getString(R.string.queen_4);
+                case 4:
+                    quote = getString(R.string.queen_4);
                     break;
-                case 5: quote=getString(R.string.queen_5);
+                case 5:
+                    quote = getString(R.string.queen_5);
                     break;
             }
             couleurPanel.setText(quote);
